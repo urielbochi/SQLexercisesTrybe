b30183d64adcafc9ae297d6105e83ff74d57ef43
Uriel <urielbochi@gmail.com>
2021-09-11 23:00:24 -0300

    Enviando exercicios bonus SQL day 3

---
+++ SQL - Dia 3/Bonus/Exercises SQL 3.sql
@@ -0,0 +1,31 @@
+USE scientists;
+
+# Escreva uma query para exibir todas as informações de todos os cientistas que possuam a letra 'e' em seu nome.
+SELECT * from scientists WHERE Name LIKE '%e%';
+
+# Escreva uma query para exibir o nome de todos os projetos cujo o código inicie com a letra A. Ordene o resultado em ordem alfabética.
+SELECT Name from projects WHERE Name LIKE 'A%' ORDER BY Name;
+
+# Escreva uma query para exibir o código e nome de todos os projetos que possuam em seu código o número 3. Ordene o resultado em ordem alfabética.
+SELECT Name, Code from projects WHERE Code LIKE '%3%';
+
+# Escreva uma query para exibir todos os cientistas (valores numéricos) cujos projetos sejam AeH3, Ast3 ou Che1.
+SELECT COUNT(Scientist) from assignedto WHERE Project IN ('AeH3', 'Ast3', 'Che1' );
+
+# Escreva uma query para exibir todas as informações de todos os projetos com mais de 500 horas
+SELECT Name, Hours from projects WHERE Hours > 500;
+
+# Escreva uma query para exibir todas as informações de todos os projetos cujas horas sejam maiores que 250 e menores 800.
+SELECT Name, Hours from projects WHERE Hours BETWEEN 250 and 800;
+
+# Escreva uma query para exibir o nome e o código de todos os projetos cujo nome NÃO inicie com a letra A.
+SELECT Name, Code from projects WHERE Name NOT LIKE 'A%';
+
+# Escreva uma query para exibir o nome de todos os projetos cujo código contenha a letra H .
+SELECT Name, Code from projects WHERE Code Like '%H%';
+
+
+
+
+
+
